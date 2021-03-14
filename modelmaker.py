19c0df029f3791735ccadfe0eff4c20c95d9d0cb
Griffoen <54778111+Griffoen@users.noreply.github.com>
2021-03-14 17:46:50 +0100

    modified the image-generator, and added an example

--- modelmaker.py
+++ modelmaker.py
@@ -4,9 +4,13 @@ Created on Fri Mar  5 20:23:46 2021
 
 @author: 20183193
 """
+"""
+refrance source:
+    https://github.com/seasonyc/densenet
+"""
 version = 'AE_v1' # naming sceme for models, prevent namingconflicts. AE = AutoEncoder
 
-#%% imports
+# imports
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
@@ -16,8 +20,7 @@ from tensorflow.keras.layers import Conv2D, MaxPool2D, UpSampling2D, Input
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 import time
-#%% Preparations
-date = time.strftime("%d-%m-%Y_%H-%M-%S")
+# Preparations
 path_images = '../../Images/' # navigate to ~/source/Images from ~/source/Github/autoencoder.py
 path_models = './models/'
 
@@ -27,46 +30,48 @@ image_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)
 
 
 def AutoEncoder(input_shape=(96,96,3), filters=[64, 32, 16], kernel_size=(3,3), pool_size=(2,2),
-                activation='relu',padding='same',
-                loss='MeanSquaredError', optimizer='adam',
-                model_name=None):
-    x = Input(shape=input_shape)
+                activation='relu',padding='same', model_name=None):
     #encoder
+    input_layer = Input(shape=input_shape)
+    x = input_layer
     for i in filters:
         x = Conv2D(i, kernel_size, activation=activation, padding=padding)(x)
         x = MaxPool2D(pool_size=pool_size, padding=padding)(x)
-    x = MaxPool2D(pool_size=pool_size, padding=padding)(x) # latent vieuw
 
     # decoder
     for i in filters[::-1]: #loop over the filter in reverse
         x = Conv2D(filters[2], kernel_size, activation=activation, padding=padding)(x)
         x = UpSampling2D(pool_size)(x)
-    x = Conv2D(input_shape[2], kernel_size, activation=activation, padding=padding)(x) # output layer
+    x = Conv2D(input_shape[2], kernel_size, activation="sigmoid", padding=padding)(x) # output layer
 
-    if model_name==None:
-        model_name=f'{version}_F{filters}_K{kernel_size}_P{pool_size}'
+    # assign the model an default name if None was given
+    r = lambda x: str(x).replace(', ','.')[1:-1] # remove the (,),[,] and replace , with .
+    model_name = model_name or f"{version}_F{ r(filters) }_K{ r(kernel_size) }_P{ r(pool_size) }"
 
-    model = Model(Input(shape=input_shape), x, name=model_name)
-    model.compile(optimizer,loss)
-    return model
+    return Model(input_layer, x, name=model_name)
 
 
-def TrainModel(model, train, validation, num_epochs, batch_size=32, save_model=True, verbose=1, save_dir='./models/'):
-    if save_model == True:
+def TrainModel(model, train, validation, num_epochs, batch_size=32,
+               loss='MeanSquaredError', optimizer='adam',
+               save_model=True, verbose=1, save_dir='./models/'):
+
+    if save_model:
         model_json = model.to_json() # serialize model to JSON
-        with open(model.name+'.json', 'w') as json_file:
+        with open(save_dir + model.name+'.json', 'w') as json_file:
             json_file.write(model_json)
 
         # prepare to save wheights
         date = time.strftime("%d-%m-%Y_%H-%M-%S")
-        callbacks = ModelCheckpoint(model.name+f'_W_D{date}.hdf5', monitor='val_loss', verbose=verbose, save_best_only=True, mode='min')
+        callbacks = ModelCheckpoint(save_dir + model.name+f'_W_D{date}.hdf5', monitor='val_loss', verbose=verbose, save_best_only=True, mode='min')
     else:
         callbacks=None
 
+    model.compile(optimizer,loss)
     model_history = model.fit(train, epochs=num_epochs, batch_size=32, verbose=verbose,
                                 validation_data=validation,
                                 callbacks=callbacks)
     return model_history
+
 
 def LoadModel(name=None, path_models='./models/'):
     """
@@ -80,7 +85,7 @@ def LoadModel(name=None, path_models='./models/'):
         - old models trained pre-namechange
         - AE_v1_* : models of the new name sceme
     """
-    if not(name):
+    if name == None:
         files = [path_models + x for x in os.listdir(path_models) if x.endswith(".hdf5")]
         name = max(files , key = os.path.getctime).replace(path_models,"")  # cut off the path_models, to remain with a name
 
@@ -91,47 +96,113 @@ def LoadModel(name=None, path_models='./models/'):
             model_path  = path_models + name[:i1] + '.json'
             weight_path = path_models + name
         else: # this one contains no wheights, find latest wheights available
-            name.replace('.json','') # remove json if it was present
+            name = name.replace('.json','') # remove json if it was present
             model_path  = path_models + name + '.json'
 
             files = [path_models + x for x in os.listdir(path_models) if x.endswith(".hdf5") and x.startswith(name)]
             weight_path = max(files , key = os.path.getctime)
-    else:
+    elif name[:2].isdecimal():
         # the model is of the old type:
-        name.replace('_w.hdf5','')
-        name.replace('.json','')
-        model_path  = name + ".json"
-        weight_path = name + "_w.hdf5"
+        name = name.replace('_w.hdf5','')
+        name = name.replace('.json','')
+        model_path  = path_models + name + ".json"
+        weight_path = path_models + name + "_w.hdf5"
+    else:
+        raise Exception(f"could not indentify a valid model, check if the folowing model exists: {name}")
 
-    #%% load model and model weights
-    with open(model_path+'.json', 'r') as json_file:
+    # load model and model weights
+    with open(model_path, 'r') as json_file:
         loaded_model_json = json_file.read()
     model = model_from_json(loaded_model_json)
     model.load_weights(weight_path)
 
     return model
 
+def ImageGenerators(base_dir, train_batch_size=32, val_batch_size=32, IMAGE_SIZE=96, validation_split=0.3):
+     """
+     create four genarators:
+     train, validation, test_healthy, test_diseased
 
-def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32, classes =['0'], class_mode='input'):
+     the train and validaion set are build from the split training set.
+     the test sets are the original validaion set.
+
+     """
      # dataset parameters
-     train_path = os.path.join(base_dir, 'train+val', 'train')
-     valid_path = os.path.join(base_dir, 'train+val', 'valid')
+     train_path = base_dir + 'train+val/train'
+     valid_path = base_dir + 'train+val/valid'
 
      RESCALING_FACTOR = 1./255
 
-     # instantiate data generators
-     datagen = ImageDataGenerator(rescale=RESCALING_FACTOR)
+     # we split the training set into training and validation. The test set is made from the validaion set
+     datagen_train = ImageDataGenerator(rescale=RESCALING_FACTOR, validation_split=validation_split)
+     datagen_test = ImageDataGenerator(rescale=RESCALING_FACTOR)
 
-     train_gen = datagen.flow_from_directory(train_path,
+    # using the method from here might be better:
+    # https://towardsdatascience.com/addressing-the-difference-between-keras-validation-split-and-sklearn-s-train-test-split-a3fb803b733
+
+     train_gen = datagen_train.flow_from_directory(train_path,
                                              target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                              batch_size=train_batch_size,
-                                             classes=classes,
-                                             class_mode=class_mode)
+                                             subset='training',
+                                             classes='0',
+                                             class_mode='input')
 
-     val_gen = datagen.flow_from_directory(valid_path,
+     val_gen = datagen_train.flow_from_directory(train_path,
                                              target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                              batch_size=val_batch_size,
-                                             classes=classes,
-                                             class_mode=class_mode)
+                                             classes='0',
+                                             subset='validation',
+                                             class_mode='input')
 
-     return train_gen, val_gen
+     # the test generators
+     test_gen_H = datagen_test.flow_from_directory(valid_path,
+                                             target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                             batch_size=val_batch_size,
+                                             classes='0',
+                                             class_mode=None)
+
+     test_gen_D = datagen_test.flow_from_directory(valid_path,
+                                             target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                             batch_size=val_batch_size,
+                                             classes='1',
+                                             class_mode=None)
+
+     return train_gen, val_gen, test_gen_H, test_gen_D
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import numpy as np
+    train_gen, val_gen, test_gen_H, test_gen_D = ImageGenerators(path_images)
+    if False:
+        model = AutoEncoder()
+        num_epochs = 1
+        history = TrainModel(model, train_gen, val_gen, num_epochs)
+    else:
+        model = LoadModel()
+    #model.summary()
+    #%% visualize:
+    n = 10
+
+    images = test_gen_H.next().astype(np.float32)
+
+    decoded_imgs =  model.predict(images)
+
+    print("hihi")
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # display original
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(images[i])
+        #plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, n, i+1+n)
+        plt.imshow(decoded_imgs[i])
+        #plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+
