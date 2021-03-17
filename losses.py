a2c27d189fc81f41872ac80b4c71813ee5a07cc5
Neen137 <r.j.stoffijn@student.tue.nl>
2021-03-17 12:08:11 +0100

    Losses geschreven met NCC en MSE, NCC loopt alleen op met meer correlatie, dus het is een winst function ipv loss

---
+++ losses.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy.signal
+
+def MSE_rgb(im1,im2):
+    '''Input: two NxNx3 arrays
+    Output: mean squared error value averaged for every color channel
+    '''
+    err_array = [0,0,0]
+    for i in range(3):
+        err = np.sum((im1[:,:,i] - im2[:,:,i]) ** 2)
+        err = err / im1[:,:,i].shape[0] * im1[:,:,i].shape[1]
+        err_array[i] = err
+
+    mse = np.mean(err_array)
+    return mse
+
+
+
+def NCC_rgb(im1,im2):
+    '''Input: two NxNx3 arrays
+    Output: mean of NCC values for all three color channels
+    '''
+    
+
+    cc_array= [1,1,1]
+
+
+    for i in range(im1.shape[2]):
+        nim1 = im1[:,:,i] - im1[:,:,i].mean()
+        nim2 = im2[:,:,i] - im1[:,:,i].mean()
+        cc_array[i] = scipy.signal.correlate2d(nim1,nim2, mode = "valid")
+
+    cc = np.mean(cc_array)
+    return cc
+
+
