b96f85501f304b571814289cf80b69dc99ed4830
Neen137 <r.j.stoffijn@student.tue.nl>
2021-03-18 11:51:09 +0100

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy.signal
 import modelmaker
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+from tensorflow.keras.applications.vgg19 import VGG19
 
 def MSE_rgb(im1,im2):
     '''Input: two NxNx3 arrays
@@ -34,7 +37,64 @@ def NCC_rgb(im1,im2):
     cc = np.mean(cc_array)
     return cc
 
-#%%
+
+
+def perceptual_loss(input_images, reconstructed_images):
+    """
+    Calculates the relative perceptual L1 loss between an original image and its reconstructed image.
+    Designed to work with the existing code in classifier.py and the PCAM image generators.
+    
+    Expected input: numpy array with shape: (number of images (batch size),X,Y,Z)
+    Output: Relative perceptual L1 loss (float)
+    """
+    batch_size = input_images.shape[0]
+    image_shape = input_images.shape[1:] #(96,96,3)
+
+    # retrieve vgg19 model
+    vgg19_model = VGG19(include_top=False, weights="imagenet", input_shape=image_shape)
+    vgg19_model.trainable = False
+    
+    # TO BE DONE: Calculate the following mean and std values of filter responses on our own data:
+    # 
+    # pre-calculated mean and std of the filter responses of the vgg19 model
+    vgg19_mean = np.array([0.485, 0.456, 0.406])
+    vgg19_std = np.array([0.229, 0.224, 0.225])
+     
+    features_1 = vgg19_model.predict(input_images)
+    features_2 = vgg19_model.predict(reconstructed_images)
+    
+    features_shape = features_1.shape[1:] #(3,3,512)
+    
+    # initialize losses matrix
+    losses = np.empty((batch_size,features_shape[0],features_shape[1]))
+    
+    for i in range(batch_size): 
+        f1=features_1[i]
+        f2=features_2[i]
+        
+        # initialize normalized feature matrices
+        f1_n = np.empty(features_shape)
+        f2_n = np.empty(features_shape)
+        
+        # normalization
+        for j in range(features_shape[0]):
+            f1_n[j] = (f1[j] - vgg19_mean[j])/vgg19_std[j]
+            f2_n[j] = (f2[j] - vgg19_mean[j])/vgg19_std[j]
+            
+        # calculate perceptual loss of current image
+        numerator = np.sqrt(((f1_n - f2_n)**2).sum(-1))
+        denominator = np.sqrt((f1_n**2).sum(-1))
+        
+        curr_loss = numerator/denominator
+        
+        losses[i] = curr_loss
+        
+    # calculate end-result of the perceptual losses (this is the L1 loss (I think))
+    loss = losses.sum(2).sum(1).sum(0)/losses.size
+    
+    return loss
+
+#%% loss function test
 test_gen_H, test_gen_D = modelmaker.ImageGeneratorsTest("../../Images/")
 
 images = test_gen_H.next()
