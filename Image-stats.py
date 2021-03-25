9a6deca4f9d4ab4f6d3d57e31f0b5a50337e0582
Griffoen <54778111+Griffoen@users.noreply.github.com>
2021-03-25 18:28:27 +0100

    fix for the training / validation split

--- Image-stats.py
+++ Image-stats.py
@@ -15,30 +15,45 @@ path_images = '../../Images/' # navigate to ~/source/Images from ~/source/Github
 
 paths = {'Train':         '/'.join((path_images,'train+val/train','0')),
          'Valdation':     '/'.join((path_images,'train+val/train','0')),
-         'Test Healthy':  '/'.join((path_images,'train+val/valid','0')),
-         'Test Diseased': '/'.join((path_images,'train+val/valid','1'))}
+         'Test Healthy':  '/'.join((path_images,'train+val/valid','0')),         'Test Diseased': '/'.join((path_images,'train+val/valid','1'))}
+train_split = 1 - 0.3
 
 print('[info] starting')
 for key,value in paths.items():
     print(f'[info] working on {key}')
     L_images = os.listdir(value)
-    tot = len(L_images)
+
+    tot_og = len(L_images)
+    if key == 'Train':
+        tot = int(tot_og*train_split)
+    elif key == 'Valdation':
+        tot = int(tot_og*(1-train_split))
+    else:
+        tot = tot_og
 
     mean = 0 # is an numpy image array
     for i,p in enumerate(L_images):
+        if key == 'Train' and i > train_split*tot_og:
+            break # only check the first train_split% images
+        if key == 'Valdation' and i <= train_split*tot_og:
+            continue # skip the first train_split% images
         im = PIL.Image.open(value+'/'+p)
         im = np.asarray(im)/255
         mean += im
-        print(f'\r[progres - mean] {i+1}/{tot}',end="")
+        print(f'\r[progres - mean] {i+1 -(key == "Valdation")*(tot_og-tot) }/{tot}',end="")
     mean = mean/tot
 
     "Ei(Xi - mean)^2"
     Ex_m = 0 # is an numpy image array
     for i,p in enumerate(L_images):
+        if key == 'Train' and i > train_split*tot_og:
+            break # only check the first train_split% images
+        if key == 'Valdation' and i <= train_split*tot_og:
+            continue # skip the first train_split% images
         im = PIL.Image.open(value+'/'+p)
         im = np.asarray(im)/255
         Ex_m += (im-mean)**2
-        print(f'\r[progres - Ei(xi-mean)^2] {i+1}/{tot}',end="")
+        print(f'\r[progres - Ei(xi-mean)^2] {i+1 -(key == "Valdation")*(tot_og-tot) }/{tot}',end="")
     variance =       Ex_m/tot
     sample_variace = Ex_m/(tot-1)
     standard_error = (Ex_m/(tot*(tot-1)))**0.5
@@ -52,5 +67,4 @@ for key,value in paths.items():
     t2 = t3
 t3 = time.time()
 print(f'[info] Total time used: {(t3-t1)//3600:.0f}u {(t3-t1)%3600//60:.0f}m {(t3-t1)%3600%60:.0f}s')
-
 
